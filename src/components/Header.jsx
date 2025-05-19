af3462ce8e9b99d5694388c84be696805520d2f6
Matthew Biskas <matthewbiskas@gmail.com>
2025-05-19 16:05:08 +0200

    prettier

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useState} from 'react';
 import {
     Dialog,
     DialogPanel,
@@ -9,13 +9,10 @@ import {
     PopoverButton,
     PopoverGroup,
     PopoverPanel,
-} from '@headlessui/react'
-import {
-    Bars3Icon,
-    XMarkIcon,
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import { FaDiscord, FaYoutube, FaGithub, FaTwitter, FaCoffee } from "react-icons/fa";
+} from '@headlessui/react';
+import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline';
+import {ChevronDownIcon} from '@heroicons/react/20/solid';
+import {FaDiscord, FaYoutube, FaGithub, FaTwitter, FaCoffee} from 'react-icons/fa';
 
 const pages = [
     {name: 'Home', href: '/'},
@@ -25,23 +22,23 @@ const pages = [
     {name: 'Gallery', href: '/gallery'},
     {name: 'Videos', href: '/videos'},
     {name: 'FAQ', href: '/faq'},
-]
+];
 
 const about = [
-    { name: 'Privacy Policy', href: '/about/privacy-policy' },
-    { name: 'Terms and Conditions', href: '/about/terms-and-conditions' },
-]
+    {name: 'Privacy Policy', href: '/about/privacy-policy'},
+    {name: 'Terms and Conditions', href: '/about/terms-and-conditions'},
+];
 
 const external = [
-    { name: 'YouTube', href: 'https://www.youtube.com/@gmoralistube', icon: FaYoutube},
-    { name: 'Discord', href: 'https://discord.gg/bFJxfftGW6' , icon: FaDiscord},
-    { name: 'GitHub', href: 'https://github.com/shadps4-emu/shadPS4', icon: FaGithub },
-    { name: 'X', href: 'https://twitter.com/shadps4', icon: FaTwitter },
-    { name: 'KoFi', href: 'https://ko-fi.com/shadps4', icon: FaCoffee },
-]
+    {name: 'YouTube', href: 'https://www.youtube.com/@gmoralistube', icon: FaYoutube},
+    {name: 'Discord', href: 'https://discord.gg/bFJxfftGW6', icon: FaDiscord},
+    {name: 'GitHub', href: 'https://github.com/shadps4-emu/shadPS4', icon: FaGithub},
+    {name: 'X', href: 'https://twitter.com/shadps4', icon: FaTwitter},
+    {name: 'KoFi', href: 'https://ko-fi.com/shadps4', icon: FaCoffee},
+];
 
 export default function Header({children}) {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
     return (
         <header className="bg-white">
@@ -66,12 +63,8 @@ export default function Header({children}) {
 
                 {/*Header menu*/}
                 <PopoverGroup className="hidden lg:flex lg:gap-x-8">
-                    {pages.map((item) => (
-                        <a
-                            key={item.name}
-                            href={item.href}
-                            className="text-base/6 font-semibold text-gray-900"
-                        >
+                    {pages.map(item => (
+                        <a key={item.name} href={item.href} className="text-base/6 font-semibold text-gray-900">
                             {item.name}
                         </a>
                     ))}
@@ -86,7 +79,7 @@ export default function Header({children}) {
                             className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-54 overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
                         >
                             <div className="p-4">
-                                {about.map((item) => (
+                                {about.map(item => (
                                     <div
                                         key={item.name}
                                         className="group relative flex items-center gap-x-6 rounded-lg p-2 text-base/6 hover:bg-gray-50"
@@ -105,13 +98,9 @@ export default function Header({children}) {
                 </PopoverGroup>
 
                 {/*Social media icons*/}
-                <div className="hidden lg:flex items-center gap-x-4 lg:flex-1 lg:justify-end">
-                    {external.map((item) => (
-                        <a
-                            key={item.name}
-                            href={item.href}
-                            className="text-sm/6 font-semibold text-gray-900"
-                        >
+                <div className="hidden items-center gap-x-4 lg:flex lg:flex-1 lg:justify-end">
+                    {external.map(item => (
+                        <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-gray-900">
                             <item.icon className="size-7" />
                         </a>
                     ))}
@@ -139,7 +128,7 @@ export default function Header({children}) {
                     <div className="mt-6 flow-root">
                         <div className="-my-6 divide-y divide-gray-500/10">
                             <div className="space-y-2 py-6">
-                                {pages.map((item) => (
+                                {pages.map(item => (
                                     <a
                                         key={item.name}
                                         href={item.href}
@@ -152,10 +141,13 @@ export default function Header({children}) {
                                 <Disclosure as="div" className="-mx-3">
                                     <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
                                         About
-                                        <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180" />
+                                        <ChevronDownIcon
+                                            aria-hidden="true"
+                                            className="size-5 flex-none group-data-open:rotate-180"
+                                        />
                                     </DisclosureButton>
                                     <DisclosurePanel className="mt-2 space-y-2">
-                                        {about.map((item) => (
+                                        {about.map(item => (
                                             <DisclosureButton
                                                 key={item.name}
                                                 as="a"
@@ -170,15 +162,18 @@ export default function Header({children}) {
                                 <Disclosure as="div" className="-mx-3">
                                     <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
                                         External
-                                        <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180" />
+                                        <ChevronDownIcon
+                                            aria-hidden="true"
+                                            className="size-5 flex-none group-data-open:rotate-180"
+                                        />
                                     </DisclosureButton>
                                     <DisclosurePanel className="mt-2 space-y-2">
-                                        {external.map((item) => (
+                                        {external.map(item => (
                                             <DisclosureButton
                                                 key={item.name}
                                                 as="a"
                                                 href={item.href}
-                                                className="flex-1 flex rounded-lg py-2 pr-3 pl-6 gap-x-2 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
+                                                className="flex flex-1 gap-x-2 rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
                                             >
                                                 <item.icon key={item.icon} className="size-7" />
                                                 {item.name}
@@ -192,5 +187,5 @@ export default function Header({children}) {
                 </DialogPanel>
             </Dialog>
         </header>
-    )
+    );
 }
--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useState} from 'react';
 import {
     Dialog,
     DialogPanel,
@@ -9,41 +9,52 @@ import {
     PopoverButton,
     PopoverGroup,
     PopoverPanel,
-} from '@headlessui/react'
-import {
-    Bars3Icon,
-    XMarkIcon,
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+} from '@headlessui/react';
+import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline';
+import {ChevronDownIcon, PhoneIcon, PlayCircleIcon} from '@heroicons/react/20/solid';
 import shadPS4Logo from '../assets/shadPS4Logo.png';
 
 const about = [
-    { name: 'Privacy Policy', href: '/about/privacy-policy' },
-    { name: 'Terms and Conditions', href: '/about/terms-and-conditions' },
-]
+    {name: 'Privacy Policy', href: '/about/privacy-policy'},
+    {name: 'Terms and Conditions', href: '/about/terms-and-conditions'},
+];
 
 export default function Header2({children}) {
-    const [aboutMenuOpen, setAboutMenuOpen] = useState(false)
+    const [aboutMenuOpen, setAboutMenuOpen] = useState(false);
 
     return (
         <header className="bg-white">
-            <nav aria-label="Global" className="flex h-16 bg-blue-950 items-center mx-auto p-6">
-                <div className="flex items-center gap-x-2 flex-1">
+            <nav aria-label="Global" className="mx-auto flex h-16 items-center bg-blue-950 p-6">
+                <div className="flex flex-1 items-center gap-x-2">
                     {children}
                     <span className="text-2xl font-semibold text-white">ShadPS4</span>
                 </div>
-                <div className="justify-center flex gap-8 text-center">
-                    <a className="font-semibold text-white" href="/">Home</a>
-                    <a className="font-semibold text-white" href="/forums">Forums</a>
-                    <a className="font-semibold text-white" href="/downloads">Downloads</a>
-                    <a className="font-semibold text-white" href="/compatibility">Compatibility</a>
-                    <a className="font-semibold text-white" href="/gallery">Gallery</a>
-                    <a className="font-semibold text-white" href="/videos">Videos</a>
-                    <a className="font-semibold text-white" href="/faq">FAQ</a>
+                <div className="flex justify-center gap-8 text-center">
+                    <a className="font-semibold text-white" href="/">
+                        Home
+                    </a>
+                    <a className="font-semibold text-white" href="/forums">
+                        Forums
+                    </a>
+                    <a className="font-semibold text-white" href="/downloads">
+                        Downloads
+                    </a>
+                    <a className="font-semibold text-white" href="/compatibility">
+                        Compatibility
+                    </a>
+                    <a className="font-semibold text-white" href="/gallery">
+                        Gallery
+                    </a>
+                    <a className="font-semibold text-white" href="/videos">
+                        Videos
+                    </a>
+                    <a className="font-semibold text-white" href="/faq">
+                        FAQ
+                    </a>
                     <a className="font-semibold text-white">About</a>
                 </div>
                 <div className="flex-1 text-right">03</div>
             </nav>
         </header>
-    )
+    );
 }
