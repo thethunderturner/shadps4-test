80ef835adbc5b53c7308d59528f49917062b4731
Matthew Biskas <matthewbiskas@gmail.com>
2025-11-21 14:59:55 +0100

    restructure (dynamic routing) image optimization npm run prettier

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import {defineCollection, z} from 'astro:content';
+
+const blogCollection = defineCollection({
+    type: 'content',
+    schema: ({image}) =>
+        z.object({
+            title: z.string(),
+            description: z.string(),
+            date: z.coerce.date(),
+            tags: z.array(z.string()),
+            image: image().optional(),
+        }),
+});
+
+export const collections = {
+    blog: blogCollection,
+};
