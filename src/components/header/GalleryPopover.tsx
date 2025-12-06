3da47f804b5d63123139d2cb78818316cce599e4
Matthew Biskas <matthewbiskas@gmail.com>
2025-12-06 18:53:56 +0100

    fixing popovers

---
+++ src/components/header/GalleryPopover.tsx
@@ -0,0 +1,35 @@
+import {Popover, PopoverButton, PopoverPanel} from '@headlessui/react';
+import {gallery} from '@/data/navigation.ts';
+import {MdKeyboardArrowDown} from 'react-icons/md';
+
+export default function GalleryPopover() {
+    return (
+        <Popover className="relative">
+            <PopoverButton className="group flex items-center gap-x-1 text-base/6 text-text cursor-pointer font-semibold outline-none">
+                <span>Gallery</span>
+                <MdKeyboardArrowDown
+                    className="size-5 text-text/80 transition-transform duration-200 group-data-[open]:rotate-180"
+                    aria-hidden="true"
+                    focusable="false"
+                    role="presentation"
+                />
+            </PopoverButton>
+
+            <PopoverPanel
+                anchor="bottom start"
+                transition
+                className="border-border bg-header z-50 mt-3 flex w-54 flex-col overflow-hidden rounded-lg border shadow-xl transition duration-200 ease-out data-[closed]:scale-95 data-[closed]:opacity-0"
+            >
+                <div className="flex flex-col p-2">
+                    {gallery.map(item => (
+                        <a href={item.href} role="menuitem" aria-label={item.name}>
+                            <span className="block p-2 font-semibold text-text">
+                                {item.name}
+                            </span>
+                        </a>
+                    ))}
+                </div>
+            </PopoverPanel>
+        </Popover>
+    );
+}
