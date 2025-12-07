83159e8b3a18a42b85bd299473b23f8dd8d59374
Matthew Biskas <matthewbiskas@gmail.com>
2025-12-07 23:15:42 +0100

    add other OS

--- src/components/downloads/DownloadPage.tsx
+++ src/components/downloads/DownloadPage.tsx
@@ -1,9 +1,15 @@
-import { useState, useEffect } from 'react';
 import OSCard from '@/components/downloads/OSCard.tsx';
-import {FaWindows} from 'react-icons/fa';
+import {FaWindows, FaLinux, FaApple} from 'react-icons/fa';
 
 export default function DownloadPage() {
     return (
-        <OSCard name={"Windows"} totalDownloads={420} icon={FaWindows}/>
+        <>
+            <div className="text-text font-bold">shadPS4 latest version with Qt GUI (legacy interface)</div>
+            <div className="flex w-full flex-col gap-6 lg:flex-row lg:justify-evenly">
+                <OSCard name={'Windows'} totalDownloads={420} Icon={FaWindows} />
+                <OSCard name={'Linux'} totalDownloads={1234} Icon={FaLinux} />
+                <OSCard name={'macOS'} totalDownloads={20000000} Icon={FaApple} />
+            </div>
+        </>
     );
 }
--- src/components/downloads/OSCard.tsx
+++ src/components/downloads/OSCard.tsx
@@ -2,18 +2,21 @@ import { Button, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react
 import {ChevronDownIcon} from '@heroicons/react/16/solid';
 import type {IconType} from 'react-icons';
 
-export default function OSCard({name, totalDownloads, icon}: {name: string, totalDownloads: number, icon: IconType}) {
+export default function OSCard({name, totalDownloads, Icon}: {name: string, totalDownloads: number, Icon: IconType}) {
     const versions = [
-        { id: 'v4', label: 'v0.4.0', url: '/downloads/v4.zip' },
-        { id: 'v3', label: 'v0.3.0', url: '/downloads/v3.zip' },
+        { id: 'v1', label: 'v0.1.0', url: '/downloads/v1.zip' },
         { id: 'v2', label: 'v0.2.0', url: '/downloads/v2.zip' },
+        { id: 'v3', label: 'v0.3.0', url: '/downloads/v3.zip' },
+        { id: 'v4', label: 'v0.4.0', url: '/downloads/v4.zip' },
+        { id: 'more', label: '...', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' },
     ];
 
     return(
-        <div className="flex max-h-full w-full flex-col rounded-lg bg-compat-card p-4 border-border border-2 lg:w-110">
+        <div className="flex max-h-full w-full flex-col rounded-lg bg-compat-card p-4 border-border border-2 lg:w-90">
             {/* Header */}
             <div className="flex flex-row gap-x-4 border-b border-border pb-4">
                 {/*IMPORT ICON HERE: icon*/}
+                <Icon className="size-12 fill-gray-600 dark:fill-white" />
                 <div className="flex flex-col">
                     <span className="text-xl font-normal text-gray-600 dark:text-white">{name}</span>
                     <span className="text-sm font-normal text-gray-600 dark:text-gray-300">Total Downloads: {totalDownloads}</span>
@@ -21,13 +24,13 @@ export default function OSCard({name, totalDownloads, icon}: {name: string, tota
             </div>
 
             <div className="flex flex-row gap-x-4 items-center justify-between mt-4">
-                <Button className="rounded bg-header p-2 text-sm text-text border border-border">
+                <Button className="rounded-lg bg-header p-2 text-sm text-text border border-border">
                     Download Latest
                 </Button>
 
                 {/* Replaced Select with Menu */}
                 <Menu>
-                    <MenuButton className="flex items-center gap-2 rounded bg-header p-2 text-sm text-text border border-border">
+                    <MenuButton className="w-40 flex items-center gap-2 rounded-lg bg-header p-2 text-sm text-text border border-border">
                         Official Releases
                         <ChevronDownIcon className="size-4 opacity-50" />
                     </MenuButton>
