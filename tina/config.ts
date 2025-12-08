067e66a56b0a3fcaffdcfbc1b3a96b07c8273721
Matthew Biskas <matthewbiskas@gmail.com>
2025-12-08 23:20:37 +0100

    fix media root remvoe default template comments

--- tina/config.ts
+++ tina/config.ts
@@ -4,12 +4,7 @@ import {Faq} from './collections/faq.ts';
 import {Quickstart} from './collections/quickstart.ts';
 import {About} from './collections/about.ts';
 
-// Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
+const branch = process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || "main";
 
 export default defineConfig({
     branch,
@@ -25,11 +20,10 @@ export default defineConfig({
     },
     media: {
         tina: {
-            mediaRoot: '',
+            mediaRoot: '/src/assets/images',
             publicFolder: 'public',
         },
     },
-    // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/r/content-modelling-collections/
     schema: {
         collections: [BlogPost, Faq, Quickstart, About],
     },
