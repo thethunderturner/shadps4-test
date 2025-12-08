1b8fcc4d9749a604bfcef134be26decc73827a5e
Matthew Biskas <matthewbiskas@gmail.com>
2025-12-08 23:26:39 +0100

    site config

---
+++ src/data/site.config.ts
@@ -0,0 +1,21 @@
+interface SiteConfig {
+    site: string;
+    author: string;
+    title: string;
+    description: string;
+    lang: string;
+    ogLocale: string;
+    shareMessage: string;
+    paginationSize: number;
+}
+
+export const siteConfig: SiteConfig = {
+    site: 'https://shadps4.net/', // Write here your website url
+    author: 'ShadPS4', // Site author
+    title: 'ShadPS4', // Site title.
+    description: 'ShadPS4 – PS4 emulator for PC.', // Description to display in the meta tags
+    lang: 'en-US',
+    ogLocale: 'en_US',
+    shareMessage: 'Share this post', // Message to share a post on social media
+    paginationSize: 8, // Number of posts per page
+};
