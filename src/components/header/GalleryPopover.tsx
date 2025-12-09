f4b00089d0fcc19270e53018911ea0d5fa4de041
Matthew Biskas <matthewbiskas@gmail.com>
2025-12-09 11:58:39 +0100

    TW + UI fixes

--- src/components/header/GalleryPopover.tsx
+++ src/components/header/GalleryPopover.tsx
@@ -8,7 +8,7 @@ export default function GalleryPopover() {
             <PopoverButton className="group text-text flex cursor-pointer items-center gap-x-1 text-base/6 font-semibold outline-none">
                 <span>Gallery</span>
                 <MdKeyboardArrowDown
-                    className="text-text/80 size-5 transition-transform duration-200 group-data-[open]:rotate-180"
+                    className="text-text/80 size-5 transition-transform duration-200 group-data-open:rotate-180"
                     aria-hidden="true"
                     focusable="false"
                     role="presentation"
@@ -18,7 +18,7 @@ export default function GalleryPopover() {
             <PopoverPanel
                 anchor="bottom start"
                 transition
-                className="border-border bg-header z-50 mt-3 flex w-54 flex-col overflow-hidden rounded-lg border shadow-xl transition duration-200 ease-out data-[closed]:scale-95 data-[closed]:opacity-0"
+                className="border-border bg-header z-50 mt-3 flex w-54 flex-col overflow-hidden rounded-lg border shadow-xl transition duration-200 ease-out data-closed:scale-95 data-closed:opacity-0"
             >
                 <div className="flex flex-col p-2">
                     {gallery.map(item => (
